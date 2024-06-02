89e14fff0ebddae14639d243810fbe6cb1585947
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-06-03 02:36:38 +0530

    Handling web request in golang

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to JSON video")
+	EncodeJson()
+	DecodeJson()
+}
+
+func EncodeJson () {
+
+}
+
+func DecodeJson () {
+	
+}
