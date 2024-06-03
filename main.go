ffe760e88cdf2953eb13808bceed6d032ed35fa5
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-06-03 19:06:51 +0530

    decode

--- main.go
+++ main.go
@@ -34,5 +34,34 @@ func EncodeJson () {
 }
 
 func DecodeJson () {
-	
+	jsonDataFromWeb := []byte(`
+	{
+		"coursename": "ReactJS Bootcamp",
+		"Price": 299,
+		"website": "LearnCodeOnline.in",
+		"tags": ["web-dev","js"]
+	}
+	`)
+
+	var lcoCourse course
+
+	checkValid := json.Valid(jsonDataFromWeb)
+
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("JSON WAS NOT VALID")
+	}
+
+	// some cases where you just want to add data to key value
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("Key is %v  and value is %v and Type is: %T\n", k, v, v)
+	}
 }
