209904971448c6fc25a5179d54230da48b66c37f
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-06-03 19:06:12 +0530

    encode

--- main.go
+++ main.go
@@ -5,6 +5,14 @@ import (
 	"fmt"
 )
 
+type course struct {
+	Name     string `json:"coursename"`
+	Price    int
+	Platform string   `json:"website"`
+	Password string   `json:"-"`
+	Tags     []string `json:"tags,omitempty"`
+}
+
 func main() {
 	fmt.Println("Welcome to JSON video")
 	EncodeJson()
@@ -12,7 +20,17 @@ func main() {
 }
 
 func EncodeJson () {
+	lcoCourses := []course{
+		{"ReactJS Bootcamp", 299, "LearnCodeOnline.in", "abc123", []string{"web-dev", "js"}},
+		{"MERN Bootcamp", 199, "LearnCodeOnline.in", "bcd123", []string{"full-stack", "js"}},
+		{"Angular Bootcamp", 299, "LearnCodeOnline.in", "hit123", nil},
+	}
 
+	finalJson , err := json.MarshalIndent(lcoCourses,"","\t")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", finalJson)
 }
 
 func DecodeJson () {
